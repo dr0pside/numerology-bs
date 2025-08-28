c703844a79e3233f641e805f6eb22d5a1ae820b2
dr0pside <111355189+dr0pside@users.noreply.github.com>
2025-08-29 00:02:33 +0300

    Update helpers.py
    Added cmd_print function

--- helpers.py
+++ helpers.py
@@ -2,8 +2,8 @@ MASTER_NUMBERS = [11, 22, 33]
 KARMIC_NUMBERS = [13, 14, 16, 19]
 
 full = {"A": 1, "B": 2, "C": 3, "D": 4, "E": 5, "F": 6, "G": 7, "H": 8, "I": 9,
-             "J": 1, "K": 2, "L": 3, "M": 4, "N": 5, "O": 6, "P": 7, "Q": 8, "R": 9,
-             "S": 1, "T": 2, "U": 3, "V": 4, "W": 5, "X": 6, "Y": 7, "Z": 8}
+        "J": 1, "K": 2, "L": 3, "M": 4, "N": 5, "O": 6, "P": 7, "Q": 8, "R": 9,
+        "S": 1, "T": 2, "U": 3, "V": 4, "W": 5, "X": 6, "Y": 7, "Z": 8}
 
 vowels = {"A": 1, "E": 5, "I": 9, "O": 6, "U": 3}
 
@@ -42,7 +42,8 @@ planes_matrix = {
 
 karma = []
 added_karma = []
-karma_count = 0
+karma_count = []
+
 
 def sum_digits(n, mode=""):
     s = 0
@@ -51,7 +52,8 @@ def sum_digits(n, mode=""):
         if n in MASTER_NUMBERS:
             return n
         if n in KARMIC_NUMBERS and n not in added_karma:
-            karma.append(f"{n}/{sum(int(d) for d in str(n))}") #append karmic number to list as a nicely formatted string
+            karma.append(
+                f"{n}/{sum(int(d) for d in str(n))}")  # append karmic number to list as a nicely formatted string
             added_karma.append(n)
     while n:
         s += n % 10
@@ -59,16 +61,17 @@ def sum_digits(n, mode=""):
 
     if mode != "ignore":
         if s >= 10 and s not in MASTER_NUMBERS:
-            return sum_digits(s) #recursive call in case s still has multiple digits
+            return sum_digits(s)  # recursive call in case s still has multiple digits
     else:
         if s >= 10:
             return sum_digits(s, mode="ignore")
 
     return s
 
+
 def pure_sum_letters(f_name: str, l_name: str, m_name=""):
     name_dict = full
-    
+
     for name in [f_name, l_name, m_name]:
         total = 0
         for l in name:
@@ -77,11 +80,13 @@ def pure_sum_letters(f_name: str, l_name: str, m_name=""):
                 total += name_dict[l]
     return total
 
+
 def challenge_math(n1, n2):
     res = sum_digits(n1, mode="ignore") - sum_digits(n2, mode="ignore")
     if res < 0:
         res *= -1
     return res
+
 
 def bridge(n1, n2, mode=""):
     x = n1 - n2
@@ -92,14 +97,15 @@ def bridge(n1, n2, mode=""):
     if mode == "le":
         print(f"Your Life's Path - Expression bridge is {x}")
 
+
 def sum_digits_and_print(n, mode=""):
     if mode == "maturity":
         print(f"Your Maturity number is {sum_digits(n, mode='ignore')}")
     if mode == "rational":
         print(f"Your Rational Thought number is {sum_digits(n, mode='ignore')}")
 
-def karma_print():
 
+def karma_print():
     karma_str = ""
     count = 0
     for entry in karma:
@@ -136,21 +142,24 @@ def passion_karma(f_name: str, l_name: str, d, m_name=""):
             count += 1
             if count != len(karmic_list):
                 karma_str += ", "
-        karma_count = count
+        karma_count.clear()
+        karma_count.append(count)
         return karma_str
 
     print(f"Your Hidden Passion number is {passion_number}, with {count_dict[passion_number]} occurrences.")
     print(f"Your Karmic Lesson numbers are {karmic_print()}.")
     return
 
+
 def cornerstone(f_name):
     print(f"Your Cornerstone is {f_name[0].upper()}")
 
+
 def subconscious():
-    print(f"Your Subconscious Self number is {9 - karma_count}")
+    print(f"Your Subconscious Self number is {9 - karma_count[0]}")
+
 
 def planes_of_expression(f_name, l_name, m_name=""):
-    
     planes_dict = {"Physical": 0, "Mental": 0, "Emotional": 0, "Intuitive": 0,
                    "Creative": 0, "Vacillating": 0, "Grounded": 0}
     planes_count = planes_dict.copy()
@@ -166,6 +175,21 @@ def planes_of_expression(f_name, l_name, m_name=""):
     for k, v in planes_dict.items():
         planes_dict[k] = sum_digits(v)
     for x in ['Physical', 'Mental', 'Emotional', 'Intuitive']:
-      print(f"Your {x} Plane of Expression is {planes_dict[x]}.\n")
+        print(f"Your {x} Plane of Expression is {planes_dict[x]}.\n")
 
+def cmd_print(mode):
 
+    if mode == "f_name":
+        return "Enter your first name:"
+    if mode == "m_name":
+        return "Enter your middle name:"
+    if mode == "l_name":
+        return "Enter your last name:"
+    if mode == "n_name":
+        return "Enter your nickname, if you have one:"
+    if mode == "d":
+        return "Enter your day of birth:"
+    if mode == "m":
+        return "Enter your month of birth:"
+    if mode == "y":
+        return "Enter your year of birth:"
