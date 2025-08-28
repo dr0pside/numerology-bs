84729da7eb7a26fca7e5a271ee52c2c47cffb59a
dr0pside <111355189+dr0pside@users.noreply.github.com>
2025-08-28 14:59:30 +0300

    Update numerolovrajeala.py
    Capitalised constants

--- numerolovrajeala.py
+++ numerolovrajeala.py
@@ -1,87 +1,210 @@
-MASTER_NUMBERS = [11, 22, 33]
-KARMIC_NUMBERS = [13, 14, 16, 19]
+import helpers as h
+from datetime import datetime
+from calendar import isleap
 
-full = {"A": 1, "B": 2, "C": 3, "D": 4, "E": 5, "F": 6, "G": 7, "H": 8, "I": 9,
-             "J": 1, "K": 2, "L": 3, "M": 4, "N": 5, "O": 6, "P": 7, "Q": 8, "R": 9,
-             "S": 1, "T": 2, "U": 3, "V": 4, "W": 5, "X": 6, "Y": 7, "Z": 8}
+#VAR INIT:
+karma = h.karma
+added_karma = h.added_karma
 
-vowels = {"A": 1, "E": 5, "I": 9, "O": 6, "U": 3}
+#MAIN FUNCS:
 
-consonants = {"B": 2, "C": 3, "D": 4, "F": 6, "G": 7, "H": 8,
-              "J": 1, "K": 2, "L": 3, "M": 4, "N": 5, "P": 7, "Q": 8, "R": 9,
-              "S": 1, "T": 2, "V": 4, "W": 5, "X": 6, "Y": 7, "Z": 8}
+def birthday(d, m, y):
+    res = ()
 
-karma = []
-added_karma = []
+    for x in [d, m, y]:
 
-def sum_digits(n, mode=""):
-    s = 0
+        if x in h.MASTER_NUMBERS:
+            res += (x,)
+        else:
+            res += (h.sum_digits(x),)
 
-    if mode != "ignore":
-        if n in MASTER_NUMBERS:
-            return n
-        if n in KARMIC_NUMBERS and n not in added_karma:
-            karma.append(f"{n}/{sum(int(d) for d in str(n))}") #append karmic number to list as a nicely formatted string
-            added_karma.append(n)
-    while n:
-        s += n % 10
-        n //= 10
+    life_path = sum(res)
 
-    if mode != "ignore":
-        if s >= 10 and s not in MASTER_NUMBERS:
-            return sum_digits(s) #recursive call in case s still has multiple digits
+    if life_path not in h.MASTER_NUMBERS and life_path >= 10:
+        life_path = h.sum_digits(life_path)
+
+    life_karma = karma.copy()
+
+    period_print = (f"  Your First Period Cycle is {h.sum_digits(m)}.\n"
+                    f"  Your Second Period Cycle is {h.sum_digits(d)}.\n"
+                    f"  Your Third Period Cycle is {h.sum_digits(y)}.\n")
+
+    pinnacle1 = h.sum_digits(h.sum_digits(m) + h.sum_digits(d))
+    pinnacle2 = h.sum_digits(h.sum_digits(d) + h.sum_digits(y))
+    pinnacle3 = h.sum_digits(pinnacle1 + pinnacle2)
+    pinnacle4 = h.sum_digits(h.sum_digits(m) + h.sum_digits(y))
+
+    pinnacle_print = (f"  Your First Pinnacle Cycle is {pinnacle1}.\n"
+                      f"  Your Second Pinnacle Cycle is {pinnacle2}.\n"
+                      f"  Your Third Pinnacle Cycle is {pinnacle3}.\n"
+                      f"  Your Fourth Pinnacle Cycle is {pinnacle4}.\n")
+
+    challenge1 = h.challenge_math(d, m)
+    challenge2 = h.challenge_math(y, d)
+    challenge3 = h.challenge_math(challenge1, challenge2)
+    challenge4 = h.challenge_math(y, m)
+
+    challenge_print = (f"  Your First Challenge number is {challenge1}.\n"
+                       f"  Your Second Challenge number is {challenge2}.\n"
+                       f"  Your Third Challenge number is {challenge3}.\n"
+                       f"  Your Fourth Challenge number is {challenge4}.\n")
+
+    sun_number = h.sum_digits(d + m, mode="ignore")
+
+    if len(life_karma) == 0:
+        print(
+            f"\nYour Life Path is {life_path}, with no karmic debt numbers.\n{period_print}\n{pinnacle_print}\n{challenge_print}")
     else:
-        if s >= 10:
-            return sum_digits(s, mode="ignore")
+        print(
+            f"\nYour Life Path is {life_path}, with the karmic debt(s) {h.karma_print()}.\n{period_print}\n{pinnacle_print}\n{challenge_print}")
 
-    return s
+    print(f"Your Sun number is {sun_number}.")
 
-def challenge_math(n1, n2):
-    res = sum_digits(n1, mode="ignore") - sum_digits(n2, mode="ignore")
-    if res < 0:
-        res *= -1
-    return res
+def name_analysis(f_name: str, l_name: str, m_name="", mode=""):
+    name_dict = h.full
 
-def karma_print():
+    func_str = "Expression"
 
-    karma_str = ""
-    count = 0
-    for entry in karma:
-        karma_str += entry
-        count += 1
-        if count != len(karma):
-            karma_str += ", "
-    return karma_str
+    if mode == "soul":
+        name_dict = h.vowels
+        func_str = "Heart's Desire/Soul Urge"
 
+    if mode == "personality":
+        name_dict = h.consonants
+        func_str = "Personality"
 
-def passion_karma(f_name: str, l_name: str, d, m_name=""):
-    master_val_list = []
+    if mode == "passion":
+        h.passion_karma(f_name, l_name, name_dict, m_name)
+        return
+
+    if mode == "balance":
+        func_str = "Balance"
+
+    name_numbers = []
     for name in [f_name, l_name, m_name]:
-        val_list = []
+        total = 0
         for l in name:
             l = l.upper()
-            if l in d:
-                val_list.append(d[l])
-        master_val_list += val_list
-    count_dict = {}
-    for x in range(1, 10):
-        count_dict.update({x: 0})
-    for n in master_val_list:
-        count_dict[n] += 1
-    max_val_list = [key for (key, value) in count_dict.items() if value == max(count_dict.values())]
-    karmic_list = [key for (key, value) in count_dict.items() if value == 0]
-    passion_number = max(max_val_list)
+            if l in name_dict:
+                total += name_dict[l]
+            if mode == "balance":
+                break  # we only want the name initials for the balance number
+        name_numbers.append(h.sum_digits(total))
 
-    def karmic_print():
-        karma_str = ""
-        count = 0
-        for entry in karmic_list:
-            karma_str += str(entry)
-            count += 1
-            if count != len(karmic_list):
-                karma_str += ", "
-        return karma_str
+    expr_number = sum(name_numbers)
 
-    print(f"Your Passion number is {passion_number}, with {count_dict[passion_number]} occurrences.")
-    print(f"Your Karmic Lesson numbers are {karmic_print()}.")
-    return
+    if mode == "":
+        if len(h.karma) == 0:
+            print(f"Your {func_str} number is {h.sum_digits(expr_number)}, with no karmic debt numbers.")
+        else:
+            print(f"Your {func_str} number is {h.sum_digits(expr_number)}, with the karmic debt(s) {h.karma_print()}.")
+    else:
+        print(f"Your {func_str} number is {h.sum_digits(expr_number)}.")
+
+#RUNNER:
+
+def number_generator(f_name, l_name, m_name, d, m, y,):
+
+    h.karma.clear()
+    h.added_karma.clear()
+    birthday(d, m, y)
+    name_analysis(f_name, l_name, m_name)
+    for x in ["soul", "personality", "passion", "balance"]:
+        name_analysis(f_name, l_name, m_name, mode=x)
+
+
+#INPUT VALIDATION:
+
+def str_validation(string):
+    def has_numbers(word):
+        checks = []
+        for x in word:
+            checks.append(x.isalpha())
+        if False in checks:
+            return True
+    if has_numbers(string):
+        print("Letters only allowed - try again.")
+        interface()
+
+def int_validation(integer, mode="", d=0, m=0):
+    
+    current_year = datetime.now().year
+
+    def has_letters(number):
+        return number.isalpha() or number.startswith("0")
+    
+    def month_check(month, d_input):
+        if month in [4, 6, 9, 11] and d_input == 31:
+            return True
+        if month == 2 and d_input > 29:
+            return True
+
+    def year_check(year, d_input, m_input):
+        if d_input == 29 and m_input == 2 and isleap(year):
+            return True
+    
+    def out_of_bounds(number, mode, d=0, m=0):
+        number = int(number)
+        if mode == "day" and number > 31 or number < 1:
+            print("Day cannot be lower than 1 or bigger than 31.")
+            return True
+        if mode == "month": 
+            if number > 12 or number < 1:
+                print(f"Month cannot be lower than 1 or bigger than 12.")
+                return True
+            if month_check(number, d):
+                print(f"Month number {number} does not have that many days!")
+                return True
+        if mode == "year":
+            if number > current_year:
+                print(f"Year cannot be in the future.")
+                return True
+            if year_check(number, d, m):
+                print(f"Year {number} is not a leap year!")
+                return True
+        
+            return False
+            
+    if has_letters(integer):
+        print("Invalid format - please try again. Do not use letters or start number with 0.")
+            
+    elif out_of_bounds(integer, mode, d, m):
+        interface()
+
+#CMD WINDOW:
+
+def interface():
+
+    f_name = input("Enter your first name:")
+    str_validation(f_name)
+    m_name = input("Enter your middle name (optional):")
+    str_validation(m_name)
+    l_name = input("Enter your last name:")
+    str_validation(l_name)
+    
+    d = input("Enter your day of birth:")
+    int_validation(d, "day")
+    stored_d = int(d)
+    
+    m = input("Enter your month of birth:")
+    int_validation(m, "month", stored_d)
+    stored_m = int(m)
+
+    y = input("Enter your year of birth:")
+    int_validation(y, "year", stored_d, stored_m)
+    stored_y = int(y)
+    
+    number_generator(f_name, l_name, m_name, stored_d, stored_m, stored_y)
+    
+    x = input("Want to go again? Type 'YES' if so:   ")
+    if x.upper() == "YES":
+        interface()
+    else:
+        quit()
+
+if __name__ == "__main__":
+    interface()
+    input("\nPress any button to exit...")
+
+
+
